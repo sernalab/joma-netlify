07308002f27c4b23b901182d2c1496998633c8ad
Carles Serna <carles.serna@gmail.com>
2024-07-24 16:04:09 +0200

    add new block partners

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import Hero from "../components/Hero";
 import Features from "../components/Features";
 import Products from "../components/Products";
+import Partners from "../components/Partners";
 
 function HomePage() {
   return (
     <>
       <Hero />
       <Features />
+      <Partners />
       {/* <Products /> */}
     </>
   );
